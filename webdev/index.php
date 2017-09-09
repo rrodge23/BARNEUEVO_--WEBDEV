63e360eee428a8202abd5609ec321fedf02822b9
rrodge30 <fritz.barnuevo@jmc.edu.ph>
2017-09-09 15:10:25 +0800

    BARNEUVO ACTIVITT

---
+++ webdev/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="bootstrap.min.css">	
+	<title>RODGIE FRITZ BARNUEVO</title>
+</head>
+
+<body>
+	<form action="homepage.php" method="POST">
+		<div class="container">
+			<div class="row">
+			<div class="col-md-4"></div>
+			<div class="col-md-4">
+				<h2 stlye="">REGISTRATION FORM</h2>
+			</div>
+			<div class="col-md-4"></div>
+		</div>
+		<div class="row">
+			<div class="col-md-4"></div>
+	
+			<div class="form-group">
+			  <label for="usr">Username:</label>
+			  <input type="text" class="form-control" name="user">
+			</div>
+	
+			<div class="form-group">
+			  <label for="usr">Password:</label>
+			  <input type="text" class="form-control" name="pass">
+			</div>
+		
+	
+			<div class="form-group">
+			  <label for="usr">Re Enter Password:</label>
+			  <input type="text" class="form-control" name="repass">
+			</div>
+			
+			<input type="submit" name="submit" class="btn btn-primary">
+			<div class="col-md-4"></div>
+		</div>
+		</div>
+	</form>
+	
+
+	<script type="text/javascript" src="bootstrap.min.js"></script>
+</body>
+</html>
